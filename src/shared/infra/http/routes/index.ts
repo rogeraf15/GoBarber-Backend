230c536024b00af3029780d4e212f575da87acd2
Roger Fernandes <roger.afernandes@hotmail.com>
2021-01-28 18:53:11 -0300

    created routes and controllers to password, tokens repository and migration db

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import AppointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
-import UsersRouter from '@modules/users/infra/http/routes/users.routes';
-import SessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
-routes.use('/sessions', SessionsRouter);
-routes.use('/appointments', AppointmentsRouter);
-routes.use('/users', UsersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
